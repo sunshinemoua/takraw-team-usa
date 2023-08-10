5a17c619c99cfceea90cee4f4b8acca358a449fe
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-08-09 17:57:49 -0700

    created upcoming events section; reused article-card and resized and restyled for events card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -26,13 +26,23 @@ export const Header = () => {
   );
 };
 
-export const ArticleCard = ({ src, title }) => {
+export const ArticleCard = ({ src, title, className }) => {
   return (
-    <div className="flex-column h-96 w-3/4 m-4 rounded-lg bg-gray-300 transition-all ease-in-out hover:scale-105">
+    <div
+      className={
+        className === "article"
+          ? "flex-column h-96 w-3/4 m-4 rounded-lg bg-gray-300 transition-all ease-in-out hover:scale-105"
+          : "flex-column h-1/2 w-1/3 m-4 rounded-lg bg-gray-400 shadow-lg transition-all ease-in-out hover:scale-105"
+      }
+    >
       <img
         src={src}
         alt="takraw-player"
-        className="w-full h-3/4 rounded-t-lg "
+        className={
+          className === "article"
+            ? "w-full h-3/4 rounded-t-lg "
+            : "w-full h-5/6 rounded-t-lg"
+        }
       />
       <div className="flex justify-center m-4 font-semibold text-gray-700 uppercase">
         {title}
@@ -46,6 +56,15 @@ export const ArticleCard = ({ src, title }) => {
   );
 };
 
+export const UpcomingEvents = () => {
+  return (
+    <div className="flex  h-screen justify-around m-10">
+      <ArticleCard src="./images/takraw1.jpeg" title="Upcoming Event" />
+      <ArticleCard src="./images/takraw2.jpeg" title="Upcoming Event" />
+    </div>
+  );
+};
+
 export const TeamNews = () => {
   return (
     <div className="flex-column w-full h-1/2 p-6 bg-red-800">
@@ -53,9 +72,21 @@ export const TeamNews = () => {
         Team News
       </div>
       <div className="flex m-4 justify-between items-center">
-        <ArticleCard src="./images/takraw1.jpeg" title="title 1" />
-        <ArticleCard src="./images/takraw2.jpeg" title="title 2" />
-        <ArticleCard src="./images/takraw3.jpeg" title="title 3" />
+        <ArticleCard
+          className="article"
+          src="./images/takraw1.jpeg"
+          title="title 1"
+        />
+        <ArticleCard
+          className="article"
+          src="./images/takraw2.jpeg"
+          title="title 2"
+        />
+        <ArticleCard
+          className="article"
+          src="./images/takraw3.jpeg"
+          title="title 3"
+        />
       </div>
     </div>
   );
@@ -67,6 +98,7 @@ const Home = () => {
       <BackgroundVideo />
       <Header />
       <TeamNews />
+      <UpcomingEvents />
     </div>
   );
 };
