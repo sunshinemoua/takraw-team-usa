e1d283793480c2fe77019c1c817ebde738620955
Sunshine Moua <sunshinemoua@Sunshines-MacBook-Pro.local>
2023-08-10 17:56:16 -0700

    created footer; added logo; WIP resize logo and add other links

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="flex justify-center h-48 text-2xl text-white bg-red-800">
+      <img src="./images/takraw-ball-color.png" alt="takraw-ball"></img>
+    </div>
+  );
+};
+
+export default Footer;
